dab0d8b1d8dde8615ba499b32f182359bdfd6511
Kamruzzaman Forhad <forhadkamruzzaman@gmail.com>
2019-10-16 00:17:04 +0600

    Added a new Method called display to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Vs code is awasome");
+        greeting("Kamruzzaman");
+    }
+    public static void greeting(String name){
+        System.out.println("Welcome " + name);
     }
 }
