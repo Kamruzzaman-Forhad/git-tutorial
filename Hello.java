1ae02b7c8391bf1e7697f6415f81f84a5074b13f
Kamruzzaman Forhad <53790152+Kamruzzaman-Forhad@users.noreply.github.com>
2019-10-18 15:29:31 +0600

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Vs code is awasome");
-        greeting("Forhad");
     }
     public static void display(){
         System.out.println("Hurrah!! We are contribute in open source... ");
