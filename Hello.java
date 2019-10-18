f2616f200fda31cf9918e22b7ff1b7c697756f6a
Kamruzzaman Forhad <forhadkamruzzaman@gmail.com>
2019-10-18 12:22:26 +0600

    Two method override in hello.java

--- Hello.java
+++ Hello.java
@@ -1,12 +1,7 @@
 public class Hello{
     public static void main(String[] args){
         System.out.println("Vs code is awasome");
-<<<<<<< HEAD
-        greeting("Kamruzzaman");
-        System.out.println(sqr(5));
-=======
         greeting("Forhad");
->>>>>>> branchA
     }
     public static void greeting(String name){
         System.out.println("Hsot name : " + name);
